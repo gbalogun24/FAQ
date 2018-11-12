13b6b6caa9bd0d4a4870701a576748da6c4b7759
gafar <gb262@njit.edu>
2018-11-11 22:00:47 -0500

    Create user unit test and question seeder

--- app/User.php
+++ app/User.php
@@ -52,4 +52,7 @@ class User extends Authenticatable
     public function profile(){
         return $this->hasOne('App\Profile');
     }
+    public function questions(){
+        return $this->hasMany('App\Question');
+    }
 }
