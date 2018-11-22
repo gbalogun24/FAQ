02b4c15a22d73ff1025cd376f3dfc0f056523916
gafar <gb262@njit.edu>
2018-11-22 17:19:56 -0500

    Added delete question button

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -15,7 +15,10 @@
                            href="{{ route('question.edit',['id'=> $question->id])}}">
                             Edit Question
                         </a>
-
+                        {{ Form::open(['method' => 'DELETE', 'route' => ['question.destroy', $question->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="sumit" id="sumit">Delete
+                        </button>
+                        {!! Form::close() !!}
                     </div>
                 </div>
             </div>
